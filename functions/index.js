597cc18b599ae3da4908e3564bd81dd0b3156a7c
earGO <rh.edwin@gmail.com>
2019-11-24 13:37:09 -0600

    stable onLogin function

--- functions/index.js
+++ functions/index.js
@@ -5,10 +5,11 @@ const language = require('@google-cloud/language');
 const client = new language.LanguageServiceClient();
 const express = require('express');
 const app = express();
+const db = admin.firestore();
 
 const authenticate = async (req, res, next) => {
     if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
-        res.status(403).send({message:'Unauthorized on first check'});
+        res.status(403).send({uid:'Unauthorized on first check'});
         return;
     }
     const idToken = req.headers.authorization.split('Bearer ')[1];
@@ -18,21 +19,49 @@ const authenticate = async (req, res, next) => {
         next();
         return;
     } catch(e) {
-        // res.status(403).send({message:'Unauthorized on second check',idToken});
+        // res.status(403).send({uid:'Unauthorized on second check',idToken});
         next();
         return;
     }
 };
 
-app.use(authenticate);
+// app.use(authenticate);
 
-app.post('/test', async (req, res) => {
-    const message = JSON.parse(req.body).message;
+
+app.post('/onLogin', async (req, res) => {
+    console.log(req.body);
+    const usersRef = db.collection('users');
     try {
-        res.status(201).json({message: message});
+        const user = JSON.parse(req.body).user;
+        const {uid,email,displayName,photoUrl} = user;
+        let result = await usersRef.where('uid', '==', uid).get();
+        if(result.empty){
+            console.log('no such user');
+            await usersRef.doc(uid).set({
+                uid:uid,
+                email:email,
+                name:displayName,
+                avatar:photoUrl,
+            });
+            try{
+                const createdUser = await usersRef.where('uid','==',uid)
+                    .get();
+                const userData = createdUser.docs[0].data();
+                res.status(201).json({message:'created user',user:userData});
+            } catch (error){
+                console.log(error);
+                res.send(error)
+            }
+
+
+        } else {
+            const user = result.docs[0].data();
+            console.log(user);
+            res.status(201).json({mesage:'found user',user:user});
+        }
     } catch(error) {
-        console.log('Error detecting sentiment or saving message', error.message);
-        res.sendStatus(500);
+        console.log('Something is wrong with the request', error.uid);
+        res.sendStatus(500).json({message:'somethng wrong with request'});
     }
 });
 
---
+++ functions/keys.js
@@ -0,0 +1,1 @@
+module.exports = require('../../shadowrun-mobile-9b6c19be8bc7');
